1d6130d8df885d8dd0a4888ad1e3749156797cd3
chebrolu tejaswi <tejaswichebrolu@gmail.com>
2021-12-09 22:21:06 +0530

    tried displaying top 3 search results reviews

--- app.py
+++ app.py
@@ -31,43 +31,46 @@ def index():
             # searching for appropriate tag to redirect to the product link
             bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
             del bigboxes[0:3] # the first 3 members of the list do not contain relevant information, hence deleting them.
-            box = bigboxes[0] # taking the first iteration/product
-            productLink = "https://www.flipkart.com" + box.div.div.a['href'] # extracting the actual product link
+            # box = bigboxes[0] # taking the first iteration/product
+            bigboxes=bigboxes[0:3]
+            review_list=[]
+            for box in bigboxes:
+                productLink = "https://www.flipkart.com" + box.div.div.a['href'] # extracting the actual product link
             
-            prodRes = requests.get(productLink) # getting the product page from server
-            prod_html = bs(prodRes.text, "html.parser") # parsing the product page as HTML
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"}) # finding the HTML section containing the customer comments
-           
-            reviews = [] # initializing an empty list for reviews
-            #  iterating over the comment section to get the details of customer and their comments
-            for commentbox in commentboxes:
-                try:
-                    name=commentbox.find_all('p',{'class':'_2sc7ZR _2V5EHH'})[0].text
-                except:
-                    name = 'No Name'
-                    
-                try:
-                    rating = commentbox.div.div.div.div.text
-                except:
-                    rating = 'No Rating'
-                    
-                try:
-                    commentHead = commentbox.div.div.div.p.text
-                except:
-                    commentHead = 'No Comment Heading'
-                    
-                try:
-                    comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    custComment = comtag[0].div.text
-                except:
-                    custComment = 'No Customer Comment'
-                    
-                mydict = {"Product":searchString,"ProductURL":productLink, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                          "Comment": custComment} # saving that detail to a dictionary
-                reviews.append(mydict) #  appending the comments to the review list
-                
-            return render_template('results.html', reviews=reviews) # showing the review to the user
-        
+                prodRes = requests.get(productLink) # getting the product page from server
+                prod_html = bs(prodRes.text, "html.parser") # parsing the product page as HTML
+                commentboxes = prod_html.find_all('div', {'class': "_16PBlm"}) # finding the HTML section containing the customer comments
+            
+                reviews = [] # initializing an empty list for reviews
+                #  iterating over the comment section to get the details of customer and their comments
+                for commentbox in commentboxes:
+                    try:
+                        name=commentbox.find_all('p',{'class':'_2sc7ZR _2V5EHH'})[0].text
+                    except:
+                        name = 'No Name'
+                        
+                    try:
+                        rating = commentbox.div.div.div.div.text
+                    except:
+                        rating = 'No Rating'
+                        
+                    try:
+                        commentHead = commentbox.div.div.div.p.text
+                    except:
+                        commentHead = 'No Comment Heading'
+                        
+                    try:
+                        comtag = commentbox.div.div.find_all('div', {'class': ''})
+                        custComment = comtag[0].div.text
+                    except:
+                        custComment = 'No Customer Comment'
+                        
+                    mydict = {"Product":searchString,"ProductURL":productLink, "Name": name, "Rating": rating, "CommentHead": commentHead,
+                            "Comment": custComment} # saving that detail to a dictionary
+                    reviews.append(mydict) #  appending the comments to the review list
+                review_list.append(reviews)
+                print(len(review_list))
+            return render_template('results.html', review_list=review_list) # showing the review to the user
         except:
             return 'OOPS! Something is wrong.'
 
